81adb534181aca7274100a552702a744d8350f56
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-10-17 00:27:07 +0900

    feat: 設定変更を即座に反映
    ※ 拡張インストール直後はページ更新しないと反映不可

--- chrome-extension/content_scripts/forbid-file-upload.js
+++ chrome-extension/content_scripts/forbid-file-upload.js
@@ -3,39 +3,57 @@ chrome.storage.local.get({
 }, ({
 	targetDomains,
 }) => {
-	if (targetDomains.includes(location.host)) {
-		const notifyMessage = message => {
-			chrome.runtime.sendMessage({
-				method: 'notify-message',
-				message,
-			});
-		};
+	const isTargetDomain = () => targetDomains.includes(location.host);
 
-		// ファイルアップロードボタンを非表示
-		document.body.classList.add(`chrome-extension-${chrome.runtime.id}-enabled`);
+	const notifyMessage = message => {
+		chrome.runtime.sendMessage({
+			method: 'notify-message',
+			message,
+		});
+	};
 
-		document.body.addEventListener('paste', evt => {
-			if (evt.clipboardData.files.length) {
-				notifyMessage('ファイル paste は禁止されています');
-				evt.stopPropagation();
-			}
-		}, true);
+	const updateCss = () => {
+		if (isTargetDomain()) {
+			// ファイルアップロードボタンを非表示
+			document.body.classList.add(`chrome-extension-${chrome.runtime.id}-enabled`);
+		} else {
+			document.body.classList.remove(`chrome-extension-${chrome.runtime.id}-enabled`);
+		}
+	};
 
-		document.body.addEventListener('drop', evt => {
-			if (evt.dataTransfer.files.length) {
-				notifyMessage('ファイル drop は禁止されています');
-				evt.stopPropagation();
-				evt.preventDefault();
-			}
-		}, true);
+	updateCss();
+
+	document.body.addEventListener('paste', evt => {
+		if (!isTargetDomain()) return;
+		if (evt.clipboardData.files.length) {
+			notifyMessage('ファイル paste は禁止されています');
+			evt.stopPropagation();
+		}
+	}, true);
+
+	document.body.addEventListener('drop', evt => {
+		if (!isTargetDomain()) return;
+		if (evt.dataTransfer.files.length) {
+			notifyMessage('ファイル drop は禁止されています');
+			evt.stopPropagation();
+			evt.preventDefault();
+		}
+	}, true);
 
 
-		document.body.addEventListener('click', evt => {
-			if (evt.path.some(e => e.id === 'primary_file_button')) {
-				notifyMessage('ファイル アップロード は禁止されています');
-				evt.stopPropagation();
-				evt.preventDefault();
-			}
-		}, true);
-	}
+	document.body.addEventListener('click', evt => {
+		if (!isTargetDomain()) return;
+		if (evt.path.some(e => e.id === 'primary_file_button')) {
+			notifyMessage('ファイル アップロード は禁止されています');
+			evt.stopPropagation();
+			evt.preventDefault();
+		}
+	}, true);
+
+	chrome.storage.onChanged.addListener((changes, areaName) => {
+		if (areaName !== 'local') return;
+		if (!changes.targetDomains) return;
+		targetDomains = changes.targetDomains.newValue || [];
+		updateCss();
+	});
 });
