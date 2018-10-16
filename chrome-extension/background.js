97d90fe73ebd2b519bee8716dff100d1f91e9955
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-10-16 19:11:33 +0900

    feat: アップロード禁止機能を有効にするSlackのドメインを選択できる機能を追加
    右クリックメニューから「このSlackへのファイルアップロードを禁止する」
    
    TODO: 禁止済みのページでも禁止するメニューが表示される問題に対応

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -9,3 +9,85 @@ chrome.runtime.onMessage.addListener((message) => {
 		});
 	}
 });
+
+
+const ContextMenusId = {
+	REGISTRATION: 'registration',
+	DEREGISTRATION: 'deregistration',
+};
+
+const updateContextMenu = (currentTargetDomains) => {
+	chrome.contextMenus.removeAll(() => {
+		chrome.contextMenus.create({
+			id: ContextMenusId.REGISTRATION,
+			title: 'このSlackへのファイルアップロードを禁止する',
+			documentUrlPatterns: [
+				'*://*.slack.com/*',
+			],
+		});
+		chrome.contextMenus.create({
+			id: ContextMenusId.DEREGISTRATION,
+			title: 'このSlackへのファイルアップロード禁止を解除する',
+			documentUrlPatterns: currentTargetDomains.concat('dummy').map(domain => `*://${domain}/*`),
+		});
+	});
+};
+
+chrome.contextMenus.onClicked.addListener(info => {
+	const url = new URL(info.pageUrl);
+	if (info.menuItemId === ContextMenusId.REGISTRATION) {
+		TargetDomains.add(url.host).then(updateContextMenu);
+		return;
+	}
+	if (info.menuItemId === ContextMenusId.DEREGISTRATION) {
+		TargetDomains.remove(url.host).then(updateContextMenu);
+		return;
+	}
+});
+
+const TargetDomains = (() => {
+	let promise = Promise.resolve();
+	const key = 'targetDomains';
+	const load = () => {
+		return promise = promise.then(() => {
+			return new Promise(resolve => {
+				chrome.storage.local.get({
+					[key]: [],
+				}, items => {
+					resolve(items[key]);
+				});
+			});
+		});
+	};
+	const add = domain => {
+		return promise = load().then(targetDomains => {
+			const newTargetDomains = targetDomains.filter(d => d !== domain).concat(domain);
+			return new Promise(resolve => {
+				chrome.storage.local.set({
+					[key]: newTargetDomains,
+				}, () => {
+					resolve(newTargetDomains);
+				});
+			});
+		});
+	};
+	const remove = domain => {
+		return promise = load().then(targetDomains => {
+			const newTargetDomains = targetDomains.filter(d => d !== domain);
+			return new Promise(resolve => {
+				chrome.storage.local.set({
+					[key]: newTargetDomains,
+				}, () => {
+					resolve(newTargetDomains);
+				});
+			});
+		});
+	};
+	return {
+		load,
+		add,
+		remove,
+	};
+})();
+
+TargetDomains.load().then(updateContextMenu);
--- chrome-extension/content_scripts/forbid-file-upload.js
+++ chrome-extension/content_scripts/forbid-file-upload.js
@@ -1,30 +1,41 @@
-const notifyMessage = message => {
-	chrome.runtime.sendMessage({
-		method: 'notify-message',
-		message,
-	});
-};
+chrome.storage.local.get({
+	targetDomains: [],
+}, ({
+	targetDomains,
+}) => {
+	if (targetDomains.includes(location.host)) {
+		const notifyMessage = message => {
+			chrome.runtime.sendMessage({
+				method: 'notify-message',
+				message,
+			});
+		};
 
-document.body.addEventListener('paste', evt => {
-	if (evt.clipboardData.files.length) {
-		notifyMessage('ファイル paste は禁止されています');
-		evt.stopPropagation();
+		// ファイルアップロードボタンを非表示
+		document.body.classList.add(`chrome-extension-${chrome.runtime.id}-enabled`);
+
+		document.body.addEventListener('paste', evt => {
+			if (evt.clipboardData.files.length) {
+				notifyMessage('ファイル paste は禁止されています');
+				evt.stopPropagation();
+			}
+		}, true);
+
+		document.body.addEventListener('drop', evt => {
+			if (evt.dataTransfer.files.length) {
+				notifyMessage('ファイル drop は禁止されています');
+				evt.stopPropagation();
+				evt.preventDefault();
+			}
+		}, true);
+
+
+		document.body.addEventListener('click', evt => {
+			if (evt.path.some(e => e.id === 'primary_file_button')) {
+				notifyMessage('ファイル アップロード は禁止されています');
+				evt.stopPropagation();
+				evt.preventDefault();
+			}
+		}, true);
 	}
-}, true);
-
-document.body.addEventListener('drop', evt => {
-	if (evt.dataTransfer.files.length) {
-		notifyMessage('ファイル drop は禁止されています');
-		evt.stopPropagation();
-		evt.preventDefault();
-	}
-}, true);
-
-
-document.body.addEventListener('click', evt => {
-	if (evt.path.some(e => e.id === 'primary_file_button')) {
-		notifyMessage('ファイル アップロード は禁止されています');
-		evt.stopPropagation();
-		evt.preventDefault();
-	}
-}, true);
+});
