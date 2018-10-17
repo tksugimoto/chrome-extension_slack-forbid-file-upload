79e09d7c4cb013a87901989bfd80d201fb000b59
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-10-18 00:19:21 +0900

    feat: 拡張インストール時に開いているSlackタブをページ更新無しに機能有効化

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,4 +1,21 @@
 
+chrome.runtime.onInstalled.addListener(details => {
+	if (details.reason !== 'install') return;
+	// インストール時に既存のタブで実行する
+	chrome.tabs.query({
+		url: '*://*.slack.com/*',
+	}, tabs => {
+		tabs.forEach(tab => {
+			chrome.tabs.insertCSS(tab.id, {
+				file: 'css/change-file-button-cursor.css',
+			});
+			chrome.tabs.executeScript(tab.id, {
+				file: 'content_scripts/forbid-file-upload.js',
+			});
+		});
+	});
+});
+
 chrome.runtime.onMessage.addListener((message) => {
 	if (message.method === 'notify-message') {
 		chrome.notifications.create({
